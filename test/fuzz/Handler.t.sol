d14c9d683825c05eb62daffeb293aee2eba409d4
Laureano4656 <laureanogarciadimartino@gmail.com>
2026-02-01 03:14:04 +0000

    feat: Update invariant tests and enhance Handler contract for collateral management

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -4,14 +4,92 @@ pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+import {MockV3Aggregator} from "../../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
-contract Handler {
-    // Dummy implementations for illustration purposes
-    function protocolIsSolvent() public view returns (bool) {
-        return true;
+contract Handler is Test {
+    DSCEngine public dscEngine;
+    DecentralizedStableCoin public dsc;
+    ERC20Mock public weth;
+    ERC20Mock public wbtc;
+    MockV3Aggregator public ethUsdPriceFeed;
+    MockV3Aggregator public btcUsdPriceFeed;
+
+    uint256 public constant MAX_DEPOSIT_AMOUNT = type(uint96).max;
+
+    uint256 public timesMintInvoked;
+    address[] public usersWithCollateralDeposited;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        dscEngine = _dscEngine;
+        dsc = _dsc;
+
+        address[] memory tokens = dscEngine.getCollateralTokens();
+        weth = ERC20Mock(tokens[0]);
+        wbtc = ERC20Mock(tokens[1]);
+        timesMintInvoked = 0;
+
+        ethUsdPriceFeed = dscEngine.getCollateralTokenPriceFeed(address(weth));
+        btcUsdPriceFeed = dscEngine.getCollateralTokenPriceFeed(address(wbtc));
     }
 
-    function totalSupplyEqualsTotalDebt() public view returns (bool) {
-        return true;
+    function depositCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        amountCollateral = bound(amountCollateral, 1, MAX_DEPOSIT_AMOUNT);
+        vm.startPrank(msg.sender);
+        collateral.mint(msg.sender, amountCollateral);
+        collateral.approve(address(dscEngine), amountCollateral);
+        dscEngine.depositCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+        usersWithCollateralDeposited.push(msg.sender);
+    }
+
+    function redeemCollateral(uint256 collateralSeed, uint256 amountCollateral) public {
+        ERC20Mock collateral = _getCollateralFromSeed(collateralSeed);
+        uint256 maxCollateralToRedeem = dscEngine.getCollateralBalanceOfUser(msg.sender, address(collateral));
+        amountCollateral = bound(amountCollateral, 0, maxCollateralToRedeem);
+        if (amountCollateral == 0) {
+            return;
+        }
+        vm.startPrank(msg.sender);
+        dscEngine.redeemCollateral(address(collateral), amountCollateral);
+        vm.stopPrank();
+    }
+    // function updateCollateralPrice(uint96 newPrice) public{
+    //     int256 newPriceInt = int256(uint256(newPrice));
+    //     ethUsdPriceFeed.updateAnswer(newPriceInt);
+    // }
+
+    function mintDsc(uint256 amountDsc, uint256 addressSeed) public {
+        if (usersWithCollateralDeposited.length == 0) {
+            return;
+        }
+        address sender = usersWithCollateralDeposited[addressSeed % usersWithCollateralDeposited.length];
+        (uint256 totalDscMinted, uint256 collateralValueInUsd) = dscEngine.getAccountInformation(sender);
+        int256 maxDscToMint = (int256(collateralValueInUsd) / 2) - int256(totalDscMinted);
+
+        if (maxDscToMint < 0) {
+            return;
+        }
+
+        amountDsc = bound(amountDsc, 0, uint256(maxDscToMint));
+        if (amountDsc == 0) {
+            return;
+        }
+        vm.startPrank(sender);
+        dscEngine.mintDsc(amountDsc);
+        vm.stopPrank();
+        timesMintInvoked += 1;
+    }
+
+    // Helper Functions
+    function _getCollateralFromSeed(uint256 collateralSeed) internal view returns (ERC20Mock) {
+        if (collateralSeed % 2 == 0) {
+            return weth;
+        } else {
+            return wbtc;
+        }
     }
 }
--- test/fuzz/Invariants.t.sol
+++ test/fuzz/Invariants.t.sol
@@ -4,14 +4,15 @@ pragma solidity ^0.8.30;
 
 import {Test} from "forge-std/Test.sol";
 import {StdInvariant} from "forge-std/StdInvariant.sol";
-//import {Handler} from "./Handler.t.sol";
+import {Handler} from "./Handler.t.sol";
 import {DeployDSC} from "../../script/DeployDSC.s.sol";
 import {DSCEngine} from "../../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
 import {HelperConfig} from "../../script/HelperConfig.s.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {console} from "forge-std/console.sol";
 
-contract OpenInvariantsTest is StdInvariant, Test {
+contract Invariants is StdInvariant, Test {
     // Handler public handler;
     DeployDSC public deployer;
     DSCEngine public dscEngine;
@@ -19,12 +20,14 @@ contract OpenInvariantsTest is StdInvariant, Test {
     HelperConfig public config;
     address public weth;
     address public wbtc;
+    Handler public handler;
 
     function setUp() public {
         deployer = new DeployDSC();
         (dsc, dscEngine, config) = deployer.run();
         (,, weth, wbtc,) = config.activeNetworkConfig();
-        targetContract(address(dscEngine));
+        handler = new Handler(dscEngine, dsc);
+        targetContract(address(handler));
     }
 
     function invariant_protocolMustHaveMorevalueThanTotalSupply() public view {
@@ -36,9 +39,21 @@ contract OpenInvariantsTest is StdInvariant, Test {
         uint256 wethValue = dscEngine.getUsdValue(weth, totalWethDeposited);
         uint256 wbtcValue = dscEngine.getUsdValue(wbtc, totalWbtcDeposited);
 
+        console.log("WETH Value:", wethValue);
+        console.log("WBTC Value:", wbtcValue);
+        console.log("Total Supply:", totalSupply);
+        console.log("Times Mint Invoked:", handler.timesMintInvoked());
         assert(wethValue + wbtcValue >= totalSupply);
     }
 
+    function invariant_gettersShouldNotRevert() public view {
+        dscEngine.getCollateralTokens();
+        dscEngine.getAdditionalFeedPrecision();
+        dscEngine.getPrecision();
+        dscEngine.getLiquidationBonus();
+        dscEngine.getLiquidationThreshold();
+        dscEngine.getMinHealthFactor();
+    }
     function invariant_totalSupplyMatchesDebt() public view {
         //  assert(handler.totalSupplyEqualsTotalDebt());
     }
